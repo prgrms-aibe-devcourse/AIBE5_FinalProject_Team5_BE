9bdbf446ef98005ce1cfead0ca7e365dcace8e55
lee <zif000901@gmail.com>
2026-06-01 15:37:56 +0900

    refactor: 아파치 통신 엔진 적용 (커넥션 풀/타임아웃 적용) -> 향후 수정 가능

--- src/main/java/com/bootsignal/global/config/HrdApiClientConfig.java
+++ src/main/java/com/bootsignal/global/config/HrdApiClientConfig.java
@@ -1,7 +1,13 @@
 package com.bootsignal.global.config;
 
+import org.apache.hc.client5.http.config.ConnectionConfig;
+import org.apache.hc.client5.http.config.RequestConfig;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManager;
+import org.apache.hc.core5.util.Timeout;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 
 /**
@@ -12,12 +18,29 @@ public class HrdApiClientConfig {
 
     @Bean
     public RestClient hrdRestClient(RestClient.Builder builder) {
-        // 공공 API 특성상 응답이 지연될 수 있으므로 타임아웃을 여유있게 설정 (추후 재설정 예정)
-        org.springframework.http.client.SimpleClientHttpRequestFactory factory =
-                new org.springframework.http.client.SimpleClientHttpRequestFactory();
-        factory.setConnectTimeout(10000); // 연결 10초
-        factory.setReadTimeout(30000);    // 응답 대기 30초
+        // TCP 연결 수립 타임아웃 (서버가 응답 없을 때 무한 대기 방지)
+        ConnectionConfig connectionConfig = ConnectionConfig.custom()
+                .setConnectTimeout(Timeout.ofSeconds(10))
+                .build();
 
-        return builder.requestFactory(factory).build();
+        // 현재 단일 스레드 구조 (안정상 2개로 설정)
+        PoolingHttpClientConnectionManager connectionManager =
+                new PoolingHttpClientConnectionManager();
+        connectionManager.setMaxTotal(2);
+        connectionManager.setDefaultMaxPerRoute(2);
+        connectionManager.setDefaultConnectionConfig(connectionConfig);
+
+        // 풀 대기 타임아웃 + 응답 타임아웃
+        RequestConfig requestConfig = RequestConfig.custom()
+                .setConnectionRequestTimeout(Timeout.ofSeconds(10))
+                .setResponseTimeout(Timeout.ofSeconds(30))
+                .build();
+
+        var httpClient = HttpClients.custom()
+                .setConnectionManager(connectionManager)
+                .setDefaultRequestConfig(requestConfig)
+                .build();
+
+        return builder.requestFactory(new HttpComponentsClientHttpRequestFactory(httpClient)).build();
     }
 }
