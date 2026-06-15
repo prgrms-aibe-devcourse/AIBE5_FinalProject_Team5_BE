292559d83a51e35c78a02ea633a31280505759e8
yongseong123 <yammy5701@gmail.com>
2026-06-15 13:49:32 +0900

    feat: 댓글 api 구현

---
+++ src/main/java/com/bootsignal/domain/comment/dto/CommentCreateRequest.java
@@ -0,0 +1,14 @@
+package com.bootsignal.domain.comment.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+/**
+ * 댓글 작성 API에서 전달받는 댓글 내용을 검증하는 요청 DTO입니다.
+ */
+public record CommentCreateRequest(
+	@NotBlank(message = "댓글 내용은 필수입니다.")
+	@Size(max = 1000, message = "댓글 내용은 1000자 이하여야 합니다.")
+	String content
+) {
+}
